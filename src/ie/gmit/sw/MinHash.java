98acd7f54cd3c0ebc0980e26d15ea29d9b92ee52
MartinRep <repickym@gmail.com>
2017-12-30 12:32:31 +0000

    Process job DAO created

---
+++ src/ie/gmit/sw/MinHash.java
@@ -0,0 +1,17 @@
+package ie.gmit.sw;
+
+public class MinHash implements JobProcessor {
+
+	public MinHash() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Results processJob(Job job) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/ie/gmit/sw/Worker.java
+++ src/ie/gmit/sw/Worker.java
@@ -8,15 +8,17 @@ public class Worker implements Runnable
 
 	private static ArrayBlockingQueue<Job> inQueue;
 	private static ConcurrentHashMap<Integer, Results> outQueue;
+	private static ArrayBlockingQueue<String> servLog;
 	private volatile static int workerNumber = 0;
 	private int thisWorkerNumber;
 	private Job job;
-	private Results resutls;
+	private Results results;
 	
 	public Worker() 
 	{
 		inQueue = WorkersHandler.GetInQueue();
 		outQueue = WorkersHandler.GetOutQueue();
+		servLog = WorkersHandler.GetServLog();
 	}
 
 	@Override
@@ -24,15 +26,16 @@ public class Worker implements Runnable
 	{
 		thisWorkerNumber = workerNumber;
 		workerNumber++;
+		servLog.offer(String.format("Worker number %d start work on job number: %d", thisWorkerNumber, job.getJobNumber()));
 		try {
 			job = inQueue.take();
-			// Process document, make singles
-			// Compare it to the rest of documents from db
-			// add compare results into Result object
+			JobProcessor jobProcessor = new MinHash();
+			results = jobProcessor.processJob(job);
 			// add Result into outQueue
+			outQueue.put(job.getJobNumber(), results);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			servLog.offer(String.format("Worker number: %d processing job number: %d, Document: %s caused error: %s", thisWorkerNumber,
+					job.getJobNumber(), job.getTitle(), e.getMessage()));
 		}
 
 	}
