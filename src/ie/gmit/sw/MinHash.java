41fc8c2e378fda04052b8c1fa3bce84c325b93e7
MartinRep <repickym@gmail.com>
2017-12-31 15:55:31 +0000

    Retrieve documents & tidy up

--- src/ie/gmit/sw/MinHash.java
+++ src/ie/gmit/sw/MinHash.java
@@ -8,49 +8,66 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
 
 public class MinHash implements JobProcessor {
 
 	Document document;
-	int shingles = 299;
+	Results results;
+	private static String dbFile = "MinHash.d4o";
+	private int shingles = 299;
+	private static ArrayBlockingQueue<String> servLog;
+	private List<Document> documents = new ArrayList<>();
 	
 	public MinHash() 
 	{
+		servLog = WorkersHandler.getServLog();
 	}
 
 	@Override
 	public Results processJob(Job job) 
 	{
 		document = new Document(job.getTitle());
+		results = new Results(job.getJobNumber(), job.getTitle());
 		try {
+			retreiveDocuments();
 			Set<String> words = getWords(job.getDocument());
 			Set<Integer> hashes = getHashes(words);
-			Set<Integer> hashFunctions = getHashFunctions();
-			generateMinHashes(hashes, hashFunctions);
-			// Retrieve documents, HashMap<String name, Set<Integer> minHashes> from db40
+			document.setHashFunctions(getHashFunctions());
+			generateMinHashes(hashes, document.getHashFunctions());
 			// compare minHashes of every document and add each Result to Results object.
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			servLog.offer(String.format("MinHash caused exception processing %s Error: %s", job.getTitle(), e.getMessage()));
 		}
 		
 		return null;
 	}
 
+	@SuppressWarnings("deprecation")
+	private void retreiveDocuments()
+	{
+		ObjectContainer db = Db4o.openFile(dbFile);
+		Query query = db.query();
+		query.constrain(Document.class);
+		ObjectSet<Document> result = query.execute();
+		for (Object document : result)
+		{
+			documents.add((Document) document);
+		}
+	}
+	
 	private Set<String> getWords(BufferedReader document) throws IOException
 	{
 		String line;
-		List<String> tmp;
 		Set<String> allWords = new TreeSet<>();
 		while((line = document.readLine()) != null)
 		{
-			String[] words = line.split(" ");
-			System.out.println("Line Words count: " + words.length);
-			tmp = new ArrayList<String>(Arrays.asList(words));
-			allWords.addAll(tmp);
+			allWords.addAll(new ArrayList<String>(Arrays.asList(line.split(" "))));
 		}
 		return allWords;
 	}
@@ -73,12 +90,15 @@ public class MinHash implements JobProcessor {
 	{
 	    // get 299 numbers from db40 or generate random 299 numbers
 	    // Check db40 for existing set of hashFunctions. All documents has to be hashed with same HashFunctions!
-	    Set<Integer> hashes = new TreeSet<Integer>();
-	    Random r = new Random();
-	    for (int i = 0; i < shingles; i++)
-	    { //Create k random integers
-	    	hashes.add(r.nextInt());
-	    }
+		Set<Integer> hashes = new TreeSet<Integer>();
+		if(documents.isEmpty())
+		{
+			Random random = new Random();
+			for (int i = 0; i < shingles; i++)
+			{ //Create k random integers
+				hashes.add(random.nextInt());
+			}			
+		} else hashes = documents.get(0).getHashFunctions();
 	    return hashes;
 	}
 	
